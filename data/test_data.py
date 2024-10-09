70f3d22f38841cc1d2d044de88cc2d42dccfc5e7
tonimondejar <antonio.mondejar@mi.unc.edu.ar>
2024-10-09 16:55:55 -0300

    preguntas y respuestas hardcodeadas para testear distintos modelos

---
+++ data/test_data.py
@@ -0,0 +1,19 @@
+# https://lingua.com/es/ingles/lectura/
+
+text1 = "I live in a house near the mountains. I have two brothers and one sister, and I was born last. My father teaches mathematics, and my mother is a nurse at a big hospital. My brothers are very smart and work hard in school. My sister is a nervous girl, but she is very kind. My grandmother also lives with us. She came from Italy when I was two years old. She has grown old, but she is still very strong. She cooks the best food! My family is very important to me. We do lots of things together. My brothers and I like to go on long walks in the mountains. My sister likes to cook with my grandmother. On the weekends we all play board games together. We laugh and always have a good time. I love my family very much."
+
+# estoy poniendo las respuestas con I porque no especifica género y en la páginas las preguntaba en primera persona tmb.
+answers1 = ["near the mountains", "mathematics", "Italy", "my mother", "board games", "my brothers and I", "in the mountains", "hospital", "on the weekends"]
+
+
+
+text2 = "Hi! Nice to meet you! My name is John Smith. I am 19 and a student in college. I go to college in New York. My favorite courses are Geometry, French, and History. English is my hardest course. My professors are very friendly and smart. It’s my second year in college now. I love it! I live in a big house on Ivy Street. It’s near the college campus. I share the house with three other students. Their names are Bill, Tony, and Paul. We help each other with homework. On the weekend, we play football together. I have a younger brother. He just started high school. He is 14 and lives with my parents. They live on Mulberry Street in Boston. Sometimes they visit me in New York. I am happy when they visit. My Mom always brings me sweets and candy when they come. I really miss them, too!"
+
+# my family lo puse para ver si puede deducir eso de contexto para preguntar who do you miss?.
+answers2 = ["John Smith", "Geometry, French, and History", "English", "Ivy Street", "Bill, Tony, and Paul", "on the weekend", "14", "New York", "Mom", "my family"]
+
+
+
+text3 = "Every year we go to Florida. We like to go to the beach. My favorite beach is called Emerson Beach. It is very long, with soft sand and palm trees. It is very beautiful. I like to make sandcastles and watch the sailboats go by. Sometimes there are dolphins and whales in the water! Every morning we look for shells in the sand. I found fifteen big shells last year. I put them in a special place in my room. This year I want to learn to surf. It is hard to surf, but so much fun! My sister is a good surfer. She says that she can teach me. I hope I can do it!"
+
+answers3 = ["Florida", "my favorite beach", "Emerson Beach", "sandcastles", "dolphins and whlaes", "fifteen", "surf", "my sister"]
